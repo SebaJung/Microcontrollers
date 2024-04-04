0952852a009d507bab26b5876dabe3aa2baf54ee
Sebastian Jung <123838305+SebaJung@users.noreply.github.com>
2024-04-04 15:20:15 -0500

    Add files via upload

---
+++ Smart_Car_Code.c
@@ -0,0 +1,36 @@
+/*--------------------------
+Smart Car Code
+  By: Sebastian, Jon, Carlos 
+  Written: 03 April, 2024
+  I/O Pins
+  A0:	Left Line Sensor
+  A1:	Center Line Sensor
+  A2:	Right Line Sensor
+  A3:	
+  A4:	
+  A5:	
+  D0:	
+  D1:	
+  D2:	
+  D3:	Right Forward - OC2B
+  D4:	Left Control
+  D5:	Left Forward - OC0B
+  D6:	Left Reverse - OC0A
+  D7:	Right Control
+  D8:	ICR
+  D9:	
+  D10: 	
+  D11:	Right Reverse - OC2A
+  D12: 	Left Encoder
+  D13: 	Right Encoder
+--------------------------*/
+
+void setup(){
+	j
+	
+}
+
+void loop(){
+	
+	
+}
