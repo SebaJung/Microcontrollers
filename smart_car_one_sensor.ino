cd3070f501f1ced526b49973166faa29669bff1c
perezc169 <146757956+perezc169@users.noreply.github.com>
2024-04-05 01:30:40 -0500

    Update smart_car_one_sensor.ino

--- smart_car_one_sensor.ino
+++ smart_car_one_sensor.ino
@@ -7,12 +7,12 @@
   A0: Left Line Sensor
   A1: Center Line Sensor
   A2: Right Line Sensor
-  A3:
+  A3: TRIGGER PIN
   A4:
   A5:
   D0:
   D1:
-  D2: TRIGGER PIN
+  D2: 
   D3: Right Forward PWM - OCR2B
   D4: Left Motor Control
   D5: Left Forward PWM - OC0B
@@ -35,34 +35,34 @@ void setup() {
   DDRD = 0xFC;
 
   // output pin for the PWM signals of motor control
-  DDRB = 0X08;
+  DDRB = 0x08;
 
   // output pin for the trigger pin of ultrasonic sensor
-  DDRC = 0X08;
+  DDRC = 0x08;
 
   // enables the motor control
-  PORTD |= 0X90;
+  PORTD |= 0x90;
   // clears the interrupt enable on SREG
   cli();
 
   TCCR0A = 0xA1;  // Using phase-correct PWM, clear on A, clear on B
   TCCR0B = 0x01;  // with prescale value of 1
 
-  TCCR2A = 0XA1;  // Using phase-correct PWM, clear on A, clear on B
-  TCCR2B = 0X01;  // with prescale value of 1
+  TCCR2A = 0xA1;  // Using phase-correct PWM, clear on A, clear on B
+  TCCR2B = 0x01;  // with prescale value of 1
 
 
-  TCCR1A = 0X00;          // configure input capture unit register
-  TCCR1B = 0XC4;          // normal mode for everything
-  TIMSK1 = 0X21;          // input capture enabled w/ prescale of 256
+  TCCR1A = 0x00;          // configure input capture unit register
+  TCCR1B = 0xC4;          // normal mode for everything
+  TIMSK1 = 0x21;          // input capture enabled w/ prescale of 256
   // input capture enabled and output disabled
 
-  ADCSRA = 0XEF;           // ADC registers for line sensors
-  ADCSRB = 0X00;          // configure using AVCC, pin 4, ADLAR = 1
-  ADMUX = 0X64;           // w/ ADIF bit OFF
+  ADCSRA = 0xEF;           // ADC registers for line sensors
+  ADCSRB = 0x00;          // configure using AVCC, pin 4, ADLAR = 1
+  ADMUX = 0x64;           // w/ ADIF bit OFF
 
-  EICRA = 0X02;           // call interrupt on rising edge
-  EIMSK = 0X01;           // trigger on pin D2
+  EICRA = 0x02;           // call interrupt on rising edge
+  EIMSK = 0x01;           // trigger on pin D2
 
   // sets the interrupt enable on SREG
   sei();
@@ -83,9 +83,9 @@ void loop() {
   static unsigned long tHigh = 0;
 
   // send a 10 us pulse  from the trigger pin A3
-  PORTC ^= 0X08;
+  PORTC ^= 0x08;
   _delay_us(10);
-  PORTC ^= 0X08;
+  PORTC ^= 0x08;
 
   cli();
   if (capt[1] > capt[0]) {                    // calculates the period high and stores it in tHigh
@@ -100,6 +100,7 @@ void loop() {
 
   unsigned int distance = (tHigh * 17182L) / 10000;      // gets the whole number of distance
   Serial.println(distance);
+  cli();
   if (distance > 25) {        // if greater than 25 cm, full speed
     OCR0B = 255;
     OCR2B = 255;
@@ -116,7 +117,7 @@ void loop() {
     OCR0A = 0;
     OCR2A = 125;
   }
-
+sei();
   _delay_ms(120);         // reduce flickering
 }
 
@@ -127,11 +128,11 @@ ISR(TIMER1_CAPT_vect)
   if (!x)                                   // if x is not equal to its value
   {
     capt[0] = count + ICR1;
-    TCCR1B &= 0XBF;                         // clears the ICES1 bit and captures input on falling edge
+    TCCR1B &= 0xBF;                         // clears the ICES1 bit and captures input on falling edge
   } else
   {
     capt[1] = count + ICR1;
-    TCCR1B |= 0X40;                         // sets the ICES1 bit and captures input on rising edge
+    TCCR1B |= 0x40;                         // sets the ICES1 bit and captures input on rising edge
   }
   x ^= 1;                                   // toggling x to change the condition of the statement
 }
