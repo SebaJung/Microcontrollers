07e91bc7c48bd912f6ef0980662044c5dba2f1eb
perezc169 <146757956+perezc169@users.noreply.github.com>
2024-04-05 01:18:51 -0500

    Update smart_car_one_sensor.ino

--- smart_car_one_sensor.ino
+++ smart_car_one_sensor.ino
@@ -82,15 +82,15 @@ void loop() {
   unsigned long ticksBetween = 0;
   static unsigned long tHigh = 0;
 
-  // send a 10 us delay to the echo pin
+  // send a 10 us pulse  from the trigger pin A3
   PORTC ^= 0X08;
   _delay_us(10);
   PORTC ^= 0X08;
 
   cli();
-  if (capt[1] > capt[0]) {
+  if (capt[1] > capt[0]) {                    // calculates the period high and stores it in tHigh
     ticksBetween = capt[1] - capt[0];
-    tHigh = (ticksBetween << 4);
+    tHigh = (ticksBetween << 4);              // bitshifting multiplies by 16 bc of I/O prescaler
   }
   else {
     ticksBetween = capt[0] - capt[1];
@@ -100,18 +100,18 @@ void loop() {
 
   unsigned int distance = (tHigh * 17182L) / 10000;      // gets the whole number of distance
   Serial.println(distance);
-  if (distance > 25) {
+  if (distance > 25) {        // if greater than 25 cm, full speed
     OCR0B = 255;
     OCR2B = 255;
-  } else if (distance > 15) {
+  } else if (distance > 15) {    // if greater than 15 but less than 25, half speed
     OCR0B = 125;
     OCR2B = 125;
-  } else if (distance > 5) {
+  } else if (distance > 5) {    // if greater than 5 but less than 15, 1/3 speed
     OCR0B = 80;
     OCR2B = 80;
   }
-  else {
-    OCR0B = 125;
+  else {                        // less than 5 cm, turn to the right by sending signal
+    OCR0B = 125;                // to the right reverse PWM and left forwar PWM
     OCR2B = 0;
     OCR0A = 0;
     OCR2A = 125;
