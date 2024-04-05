396aa19c7816378ee6e5ba0b1b5c8d267cf4635b
perezc169 <146757956+perezc169@users.noreply.github.com>
2024-04-05 10:11:40 -0500

    Update smart_car_one_sensor.ino

--- smart_car_one_sensor.ino
+++ smart_car_one_sensor.ino
@@ -26,9 +26,6 @@
   D13: Right Wheel Encoder
 */
 
-volatile unsigned long count = 0;
-volatile unsigned long capt[2];
-
 void setup() {
 
   // clears the interrupt enable on SREG
@@ -40,121 +37,64 @@ void setup() {
   // output pin for the PWM signals of motor control
   DDRB = 0x08;
 
-  // output pin for the trigger pin of ultrasonic sensor
-  // DDRC = 0x08;
-
-  // enables the motor control
-  PORTD |= 0x90;
-  
   TCCR0A = 0xA1;  // Using phase-correct PWM, clear on A, clear on B
   TCCR0B = 0x01;  // with prescale value of 1
+  //TIMSK0 = 0x06;
 
   TCCR2A = 0xA1;  // Using phase-correct PWM, clear on A, clear on B
   TCCR2B = 0x01;  // with prescale value of 1
+  // TIMSK2 = 0x06;
 
-
-  TCCR1A = 0x00;          // configure input capture unit register
-  TCCR1B = 0xC4;          // normal mode for everything
-  TIMSK1 = 0x21;          // input capture enabled w/ prescale of 256
-  // input capture enabled and output disabled
-
-  ADCSRA = 0xEF;           // ADC registers for line sensors
-  ADCSRB = 0x00;          // configure using AVCC, pin 4, ADLAR = 1
-  ADMUX = 0x64;           // w/ ADIF bit OFF
+  PCICR = 0x02;   // activates the pin change interrupt
+  PCMSK1 = 0x30;  // on pins A4 and A5
 
   // sets the interrupt enable on SREG
   sei();
-  
-  // start the motors by triggering the forward PWM
-  OCR0B = 250;
-  OCR0A = 0;
-  OCR2B = 250;
-  OCR2A = 0;
 
-  Serial.begin(9600);
 }
 
+volatile unsigned char button = 0;
 
 void loop() {
+  // enables the motor control
+  PORTD |= 0x90;
 
-  // send a 10 us pulse  from the trigger pin A3
-  PORTD |= 0x04;
-  _delay_us(10);
-  PORTD &= 0xFB;
-
-  // declare variables for the periods
-  unsigned long ticksBetween = 0;
-  static unsigned long tHigh = 0;
-
-  cli();
-  if (capt[1] > capt[0]) {                    // calculates the period high and stores it in tHigh
-    ticksBetween = capt[1] - capt[0];
-    tHigh = (ticksBetween << 4);              // bitshifting multiplies by 16 bc of I/O prescaler
-  }
-  else {
-    ticksBetween = capt[0] - capt[1];
-    tHigh = (ticksBetween << 4);
-  }
-  sei();
-
-  unsigned int distance = (tHigh * 17182L) / 10000;      // gets the whole number of distance
-
-  speedControl(distance);
-  
-  Serial.println(distance);
-
-}
-
-void speedControl(unsigned int distance) {
-  if (distance > 25) {        // if greater than 25 cm, full speed
+  if (button == 0x00) {      // if no bumper closed, dont reverse
+    OCR0A = 0;
     OCR0B = 250;
-    OCR0A = 0;
+    OCR2A = 0;
     OCR2B = 250;
-    OCR2A = 0;
-  } else if (distance > 15) {    // if greater than 15 but less than 25, half speed
-    OCR0B = 125;
+  }
+  else if (button == 0x10) {
+    OCR0A = 125;                // if right bumper hits, reverse backwards
+    OCR0B = 0;                // if right bumper hits, dont forward
+    OCR2A = 0;                // if right bumper hits, dont reverse
+    OCR2B = 0;                // if right bumper hits, dont forward
+    _delay_ms(50);              // for 50ms then not reverse anymore
     OCR0A = 0;
-    OCR2B = 125;
+    OCR0B = 250;
+    OCR2B = 250;
+  }
+
+  else if (button == 0x20) {
+    OCR0A = 0;        // if left bumper is hit, dont reverse
+    OCR0B = 0;        // if left bumper is hit, dont forward for
+    OCR2B = 0;        // if left bumper is hit, dont forward for
+    OCR2A = 125;        // if left bumper is hit, reverse for
+    _delay_ms(50);      // 50ms and then go forward
     OCR2A = 0;
-  } else if (distance > 5) {    // if greater than 5 but less than 15, 1/3 speed
-    OCR0B = 80;
-    OCR0A = 0;
-    OCR2B = 80;
-    OCR2A = 0;
+    OCR2B = 250;
+    OCR0B = 250;
   }
-  else {                        // less than 5 cm, turn to the right by sending signal
-    OCR0B = 125;                // to the right reverse PWM and left forwar PWM
-    OCR2B = 0;
-    OCR0A = 0;
-    OCR2A = 125;
-  }
+
 
 }
-
-ISR(TIMER1_CAPT_vect)
+ISR(PCINT1_vect)
 {
-  static unsigned char x = 0;               // variable used to change between conditions
-  if (!x)                                   // if x is not equal to its value
-  {
-    capt[1] = count + ICR1;
-    TCCR1B |= 0x40;                         // sets the ICES1 bit and captures input on rising edge
-  }
-   else
-  {
-    capt[0] = count + ICR1;
-    TCCR1B &= 0xBF;                         // clears the ICES1 bit and captures input on falling edge
-  }
-  x ^= 1;                                   // toggling x to change the condition of the statement
+  unsigned char i = 0;
+  if (i == 0)
+    (button = PINC & 0xCF);
+  i ^= 1;
 }
 
-ISR(TIMER1_OVF_vect)          // keeps running total of the ticks elasped even after overflow
-{
-  count += 65536;
-}
 
-/*
-ISR(TIMER0_COMPA_vect)
-{
-
-}
-*/
