1d5bfd354393c7c3bf537fa59ba2f37e14880e25
perezc169 <146757956+perezc169@users.noreply.github.com>
2024-04-05 15:50:14 -0500

    Update test_code_for_car.ino

--- test_code_for_car.ino
+++ test_code_for_car.ino
@@ -26,10 +26,6 @@
   D13: Right Wheel Encoder
 */
 
-volatile unsigned int distance = 0;
-volatile unsigned long count = 0;
-volatile unsigned long capt[2];
-
 void setup() {
 
   // clears the interrupt enable on SREG
@@ -43,106 +39,89 @@ void setup() {
 
   TCCR0A = 0xA1;  // Using phase-correct PWM, clear on A, clear on B
   TCCR0B = 0x01;  // with prescale value of 1
-  TIMSK0 = 0x06;
+  //TIMSK0 = 0x06;
 
   TCCR2A = 0xA1;  // Using phase-correct PWM, clear on A, clear on B
   TCCR2B = 0x01;  // with prescale value of 1
-  TIMSK2 = 0x06;
+  // TIMSK2 = 0x06;
 
   PCICR = 0x02;   // activates the pin change interrupt
   PCMSK1 = 0x30;  // on pins A4 and A5
 
   // sets the interrupt enable on SREG
   sei();
-
-  // start the motors by triggering the forward PWM
-  OCR0B = 255;
-  OCR0A = 0;
-  OCR2B = 255;
-  OCR2A = 0;
-
+  PORTC |= 0x30;
 }
 
-volatile unsigned char button = 1;
+volatile unsigned char button = 0;
 
 void loop() {
   // enables the motor control
   PORTD |= 0x90;
-}
 
-
-ISR(TIMER0_COMPA_vect)      // reverse left PWM
-{
-  if (button == 0x00)       // if no bumper closed, dont reverse
+  if (button == 0x00) {      // if no bumper closed, dont reverse
     OCR0A = 0;
-
-  else if (button == 0x01) {
-    OCR0A = 125;                // if right bumper hits, reverse backwards
-    _delay_ms(50);              // for 50ms then not reverse anymore
-    OCR0A = 0;
+    OCR0B = 120;
+    OCR2A = 0;
+    OCR2B = 120;
   }
-
-  else if (button == 0x20)
-    OCR0A = 0;        // if left bumper is hit, dont reverse
-}
-
-ISR(TIMER0_COMPB_vect)    // forward left PWM
-{
-  if (button == 0x00)       // if no bumper closed, go forward
-    OCR0B = 250;
-
-  else if (button == 0x01) {
+  else if (button == 0x10) {
+    OCR0A = 120;                // if right bumper hits, reverse backwards
     OCR0B = 0;                // if right bumper hits, dont forward
-    _delay_ms(50);              // for 50ms then forard
-    OCR0B = 250;
-  }
-
-  else if (button == 0x20) {
-    OCR0B = 0;        // if left bumper is hit, dont forward for
-    _delay_ms(50);      // 50ms and then go forward
-    OCR0B = 250;
-
-  }
-}
-
-  ISR(TIMER2_COMPA_vect)      // reverse right PWM
-  {
-    if (button == 0x00)       // if no bumper closed, dont reverse 
-      OCR2A = 0;
-
-    else if (button == 0x01) {
-      OCR2A = 0;                // if right bumper hits, dont reverse
-    }
-
-    else if (button == 0x20) {
-      OCR2A = 125;        // if left bumper is hit, reverse for
-      _delay_ms(50);      // 50ms and then go forward
-      OCR2A = 0;
-
-    }
-  }
-    ISR(TIMER2_COMPB_vect)      // forward right PWM
-{
-  if (button == 0x00)       // if no bumper closed, go forward 
-    OCR2B = 250;
-
-  else if (button == 0x01) {
+    OCR2A = 80;                // if right bumper hits, dont reverse
     OCR2B = 0;                // if right bumper hits, dont forward
-    _delay_ms(50);              // for 50ms then forward
-    OCR2B = 250;
+    _delay_ms(500);              // for 50ms then not reverse anymore
+    OCR0A = 0;
+    OCR0B = 120;
+    OCR2B = 120;
   }
 
   else if (button == 0x20) {
+    OCR0A = 80;        // if left bumper is hit, dont reverse
+    OCR0B = 0;        // if left bumper is hit, dont forward for
     OCR2B = 0;        // if left bumper is hit, dont forward for
-    _delay_ms(50);      // 50ms and then go forward
-    OCR2B = 250;
-
+    OCR2A = 120;        // if left bumper is hit, reverse for
+    _delay_ms(500);      // 50ms and then go forward
+    OCR2A = 0;
+    OCR2B = 120;
+    OCR0B = 120;
   }
-}
-    ISR(PCINT1_vect)
-    {
-      unsigned char i = 0;
-      if (i == 0)
-        (button = PINC & 0xCF);
-      i ^= 1;
+  else if (button == 0x30) {
+    OCR0A = 120;        // if both bumpers is hit, reverse a lil
+    OCR0B = 0;
+    OCR2B = 0;
+    OCR2A = 120;
+    _delay_ms(500);
+    OCR2A = 120;        // rotate 90 degrees and go forward
+    OCR2B = 0;
+    OCR0B = 120;
+    _delay_ms(500);
+    OCR2A = 0;          // go forward
+    OCR2B = 120;
+    OCR0B = 120;
+    OCR0A = 0;
+    if (button == 0x30)  {
+      OCR0A = 120;        // if both bumpers is hit, reverse a lil
+      OCR0B = 0;
+      OCR2B = 0;
+      OCR2A = 120;
+      _delay_ms(500);
+      OCR2A = 120;        // rotate 90 degrees and go forward
+      OCR2B = 0;
+      OCR0B = 120;
+      _delay_ms(1500);
+      OCR2A = 0;          // go forward
+      OCR2B = 120;
+      OCR0B = 120;
+      OCR0A = 0;
     }
+  }
+
+
+}
+ISR(PCINT1_vect)
+{
+  (button = (~PINC & 0x30));
+}
+
+
