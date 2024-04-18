3ae9e83231bcdec811da4126472b5e10b5f855f3
Sebastian Jung <123838305+SebaJung@users.noreply.github.com>
2024-04-18 12:28:21 -0500

    Update Current_Iteration.ino header

--- Current_Iteration.ino
+++ Current_Iteration.ino
@@ -1,8 +1,8 @@
 /*
-  Barrier Demo With Whiskers
+  Barrier Demo With Whiskers and line sensors
   By: Carlos, Jon, Sebastian
   Written: March 22, 2024
-  Edited: April 11, 2024
+  Edited: April 18, 2024
   I/O Pins
   A0: Left Line Sensor
   A1: Center Line Sensor
