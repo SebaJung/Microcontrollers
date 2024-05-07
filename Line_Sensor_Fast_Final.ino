9167038ece5f357befe53ab3535a5ea26a9a3f1c
perezc169 <146757956+perezc169@users.noreply.github.com>
2024-05-07 18:21:36 -0500

    Update Line_Sensor_Fast_Final.ino

--- Line_Sensor_Fast_Final.ino
+++ Line_Sensor_Fast_Final.ino
@@ -1,15 +1,16 @@
+
 /*
-  Barrier Demo With Whiskers and line sensors
+  Line sensors with ammendity LCD screen
   By: Carlos, Jon, Sebastian
   Written: March 22, 2024
-  Edited: April 25, 2024
+  Edited: May 7, 2024
   I/O Pins
   A0: Left Line Sensor
   A1: Center Line Sensor
   A2: Right Line Sensor
-  A3:
-  A4: 
-  A5: 
+  A3: E PIN
+  A4: RS PIN
+  A5:
   D0:
   D1:
   D2: Right Wheel Encoder
@@ -18,13 +19,15 @@
   D5: Left Forward PWM - OC0B
   D6: Left Reverse PWM - OC0A
   D7: Right Motor Control
-  D8:
-  D9:
-  D10:
+  D8: LCD PIN DB4
+  D9: LCD PIN DB5
+  D10: LCD PIN DB6
   D11: Right Reverse PWM - OC2A
   D12: Left Wheel Encoder
-  D13:
+  D13: LCD PIN DB7
 */
+
+#include "hd44780.h"                // including the lcd library
 
 void setup() {
 
@@ -39,12 +42,11 @@ void setup() {
   TCCR0A = 0xA1;                    // Using phase-correct PWM, clear on A, clear on B
   TCCR0B = 0x01;                    // with prescale value of 1
 
-
   TCCR2A = 0xA1;                    // Using phase-correct PWM, clear on A, clear on B
   TCCR2B = 0x01;                    // with prescale value of 1
 
   ADCSRA = 0xCF;                    // auto trigger disable, prescale of 128 ADC enabled and start conversion bit on
-  ADCSRB = 0x00;                    // free-running mode
+  ADCSRB = 0x00;                    // not in free-running mode
   ADMUX = 0x40;                     // AVCC mode, 10-bit precision, analog bit A0 originally
 
   PCICR = 0x01;                     // activates the pin change interrupt
@@ -53,76 +55,80 @@ void setup() {
   EICRA = 0x02;                     // pin D2 and triggers on falling edge
   EIMSK = 0x01;                     // enables interrupts on D2
 
+  lcd_init();                       // lcd startup routine
   sei();                            // sets the interrupt enable flag on SREG
 
   PORTD |= 0x90;                    // enables the motor control
 }
 
-volatile unsigned char button = 0;
+volatile unsigned int  sregValue;
 volatile unsigned int  leftWheel = 0;
 volatile unsigned int  rightWheel = 0;
 volatile unsigned int  leftSensor = 0;
 volatile unsigned int  centerSensor = 0;
 volatile unsigned int  rightSensor = 0;
 
-
 void loop() {
-
+  lcd_clrscr();                   // clears the content of the screen
 
   unsigned int avgCount = average(leftWheel, rightWheel);    // average value of the toggles between both the wheels
   unsigned long distance = (avgCount * 105L) / 100;
-  
+
   //section below relating to line sensor:
   if (centerSensor >= 900) {
     OCR0A = 0;
-    OCR0B = 200;                      // sends the left motor forward
+    OCR0B = 250;                      // sends the left motor forward
     OCR2A = 0;
-    OCR2B = 200;                      // sends the right motor forward
-  }
-  else if (rightSensor > (leftSensor - 0)) {
-    OCR0A = 0;                    
-    OCR0B = 150;      
+    OCR2B = 250;                      // sends the right motor forward
+  } else if (rightSensor > (leftSensor - 0)) {
+    OCR0A = 0;
+    OCR0B = 150;
     OCR2B = 0;
     OCR2A = 80;                      //left reverse signal half speed of right
-  } 
-  else if ((leftSensor - 180) > rightSensor) {
+  } else if ((leftSensor - 180) > rightSensor) {
     OCR0A = 80;                      // right reverse signal half speed of left
     OCR0B = 0;
-    OCR2A = 0;                    
+    OCR2A = 0;
     OCR2B = 150;
   }
-  if ((distance >= 1900) && (centerSensor < 900)) {
-    OCR0B = 0;
+ 
+ if ((distance <= 1900 ) && (centerSensor < 900)) {     // set ammendity for three point turn
     OCR0A = 0;
-    OCR2B = 0;
+    OCR0B = 0;
     OCR2A = 0;
-    
+    OCR2B = 0;
   }
-}
 
+  lcd_puts("Odometer:");
+  char charbufferDistance[10];
+  ltoa((distance/10), charbufferDistance, 10);
+  lcd_goto(0x0A);
+  lcd_puts(charbufferDistance);
+  lcd_puts(" cm");
+
+  _delay_ms(50);
+}
 
 unsigned int average(unsigned int leftWheel, unsigned int rightWheel) {
   return ((leftWheel + rightWheel) / 2);
 }
 
-
 ISR(PCINT0_vect) {                // left wheel encoder
-  if ((button = PINB & 0x10) == 0)
+  if ((PINB & 0x10) == 0)
     leftWheel++;
 }
 
-
 ISR(INT0_vect) {                  // right wheel encoder
-  if ((button = PIND & 0x04) == 0)
+  if ((PIND & 0x04) == 0)
     rightWheel++;
 }
-
 
 ISR(ADC_vect) {
   /* Depending on which ADMUX value is set, it
       will store the value of that ADC register
       onto that sensor's variable
   */
+  sregValue = SREG;
   if (ADMUX == 0x40) {               // A0 left sensor
     leftSensor = ADC;
     ADMUX = 0x41;
@@ -133,6 +139,6 @@ ISR(ADC_vect) {
     rightSensor = ADC;
     ADMUX = 0x40;
   }
-
+  SREG = sregValue;
   ADCSRA |= 0x40;                    // start a new conversion
-}   
+}
